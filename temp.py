b8d19c89a4f0a1adec45bc8b326edf5ac8b68c70
Farid Harati <farid.harati@gmail.com>
2023-06-29 19:32:58 +0200

    second commit

--- temp.py
+++ temp.py
@@ -7,5 +7,10 @@ This is a python test file to test CI CD integreation
 def addition(x,y):
         sum = x + y
         print(sum)
+        
+def subtraction(x,y):
+    result = x - y
+    print(result)
 
 addition(1, 3)
+subtraction(5,3)
